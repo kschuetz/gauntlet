45cea7ac918ea5f52a91ff63ad0ed6dc23d3ef49
Kevin Schuetz <schuetzk@gmail.com>
2020-04-05 22:01:32 -0500

    Remove obsolete code

--- src/main/java/dev/marksman/gauntlet/PropTest.java
+++
@@ -1,7 +0,0 @@
-package dev.marksman.gauntlet;
-
-public interface PropTest<A> {
-    TestResult<A> run();
-
-    TestResult<A> runWithSeed(long seedValue);
-}
--- src/main/java/dev/marksman/gauntlet/TestRunner.java
+++
@@ -1,9 +0,0 @@
-package dev.marksman.gauntlet;
-
-class TestRunner {
-
-    static <A> TestResult<A> runTest(TestData<A> testData) {
-        return new TestResult<>(testData.getInitialSeedValue());
-    }
-
-}
