6fc6bae11f5a53035b00ef457e09fe09f5350c61
Kevin Schuetz <schuetzk@gmail.com>
2020-04-09 13:15:43 -0500

    Remove extra overloads for mustSatisfy

--- gauntlet-core/src/main/java/dev/marksman/gauntlet/DiscreteDomainTestApi.java
+++ gauntlet-core/src/main/java/dev/marksman/gauntlet/DiscreteDomainTestApi.java
@@ -5,8 +5,6 @@ import com.jnape.palatable.lambda.functions.Fn1;
 import java.time.Duration;
 import java.util.Set;
 
-import static dev.marksman.gauntlet.Prop.predicate;
-
 
 public interface DiscreteDomainTestApi<A> {
     DiscreteDomainTestApi<A> withTimeout(Duration timeout);
@@ -14,12 +12,4 @@ public interface DiscreteDomainTestApi<A> {
     DiscreteDomainTestApi<A> classifyUsing(Fn1<A, Set<String>> classifier);
 
     void mustSatisfy(Prop<A> prop);
-
-    default void mustSatisfy(Fn1<? super A, Boolean> predicate) {
-        mustSatisfy(Prop.predicate(predicate));
-    }
-
-    default void mustSatisfy(String name, Fn1<? super A, Boolean> predicate) {
-        mustSatisfy(predicate(name, predicate));
-    }
 }
--- gauntlet-core/src/main/java/dev/marksman/gauntlet/GeneratorTestApi.java
+++ gauntlet-core/src/main/java/dev/marksman/gauntlet/GeneratorTestApi.java
@@ -5,8 +5,6 @@ import com.jnape.palatable.lambda.functions.Fn1;
 import java.time.Duration;
 import java.util.Set;
 
-import static dev.marksman.gauntlet.Prop.predicate;
-
 public interface GeneratorTestApi<A> {
     GeneratorTestApi<A> withInitialSeed(long initialSeed);
 
@@ -17,12 +15,4 @@ public interface GeneratorTestApi<A> {
     GeneratorTestApi<A> classifyUsing(Fn1<A, Set<String>> classifier);
 
     void mustSatisfy(Prop<A> prop);
-
-    default void mustSatisfy(Fn1<? super A, Boolean> predicate) {
-        mustSatisfy(Prop.predicate(predicate));
-    }
-
-    default void mustSatisfy(String name, Fn1<? super A, Boolean> predicate) {
-        mustSatisfy(predicate(name, predicate));
-    }
 }
