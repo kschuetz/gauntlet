58ada7fb6ecca4a9f331a25e699b966296860690
Kevin Schuetz <schuetzk@gmail.com>
2020-04-18 22:48:41 -0500

    Simplify ShrinkResult.concat

--- gauntlet-core/src/main/java/dev/marksman/gauntlet/shrink/ShrinkResult.java
+++ gauntlet-core/src/main/java/dev/marksman/gauntlet/shrink/ShrinkResult.java
@@ -5,7 +5,6 @@ import dev.marksman.enhancediterables.ImmutableFiniteIterable;
 import dev.marksman.enhancediterables.ImmutableNonEmptyFiniteIterable;
 
 import static dev.marksman.enhancediterables.ImmutableFiniteIterable.emptyImmutableFiniteIterable;
-import static dev.marksman.gauntlet.shrink.LazyConcat.lazyConcat;
 import static dev.marksman.gauntlet.shrink.LazyCons.lazyCons;
 
 public final class ShrinkResult {
@@ -26,74 +25,22 @@ public final class ShrinkResult {
         return lazyCons(head, tailSupplier);
     }
 
-    public static <A> ImmutableFiniteIterable<A> concat(ImmutableFiniteIterable<A> xs, Fn0<ImmutableFiniteIterable<A>> other) {
-        return lazyConcat(xs, other);
+    public static <A> ImmutableFiniteIterable<A> concat(ImmutableFiniteIterable<A> xs,
+                                                        Fn0<ImmutableFiniteIterable<A>> other) {
+        return xs.concat(() -> other.apply().iterator());
     }
 
+    @SafeVarargs
     public static <A> ImmutableFiniteIterable<A> concat(ImmutableFiniteIterable<A> xs,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys1,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys2) {
-        return concat(xs, () -> concat(ys1.apply(), ys2));
-    }
-
-    public static <A> ImmutableFiniteIterable<A> concat(ImmutableFiniteIterable<A> xs,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys1,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys2,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys3) {
-        return concat(xs, () -> concat(ys1.apply(),
-                () -> concat(ys2.apply(), ys3)));
-    }
-
-    public static <A> ImmutableFiniteIterable<A> concat(ImmutableFiniteIterable<A> xs,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys1,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys2,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys3,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys4) {
-        return concat(xs, () -> concat(ys1.apply(),
-                () -> concat(ys2.apply(),
-                        () -> concat(ys3.apply(), ys4))));
-    }
-
-    public static <A> ImmutableFiniteIterable<A> concat(ImmutableFiniteIterable<A> xs,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys1,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys2,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys3,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys4,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys5) {
-        return concat(xs, () -> concat(ys1.apply(), ys2, ys3, ys4, ys5));
-    }
-
-    public static <A> ImmutableFiniteIterable<A> concat(ImmutableFiniteIterable<A> xs,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys1,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys2,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys3,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys4,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys5,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys6) {
-        return concat(xs, () -> concat(ys1.apply(), ys2, ys3, ys4, ys5, ys6));
-    }
-
-    public static <A> ImmutableFiniteIterable<A> concat(ImmutableFiniteIterable<A> xs,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys1,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys2,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys3,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys4,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys5,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys6,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys7) {
-        return concat(xs, () -> concat(ys1.apply(), ys2, ys3, ys4, ys5, ys6, ys7));
-    }
-
-    public static <A> ImmutableFiniteIterable<A> concat(ImmutableFiniteIterable<A> xs,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys1,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys2,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys3,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys4,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys5,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys6,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys7,
-                                                        Fn0<ImmutableFiniteIterable<A>> ys8) {
-        return concat(xs, () -> concat(ys1.apply(), ys2, ys3, ys4, ys5, ys6, ys7, ys8));
+                                                        Fn0<ImmutableFiniteIterable<A>> first,
+                                                        Fn0<ImmutableFiniteIterable<A>> second,
+                                                        Fn0<ImmutableFiniteIterable<A>>... more) {
+        ImmutableFiniteIterable<A> result = xs.concat(() -> first.apply().iterator())
+                .concat(() -> second.apply().iterator());
+        for (Fn0<ImmutableFiniteIterable<A>> f : more) {
+            result = result.concat(() -> f.apply().iterator());
+        }
+        return result;
     }
 
 }
