57ebe3d76e5538d5df7c550db2033618b6e6dfba
Kevin Schuetz <schuetzk@gmail.com>
2020-05-06 21:41:34 -0500

    Delombok DomainTestParameters

--- gauntlet-core/src/main/java/dev/marksman/gauntlet/DomainTestParameters.java
+++ gauntlet-core/src/main/java/dev/marksman/gauntlet/DomainTestParameters.java
@@ -3,24 +3,25 @@ package dev.marksman.gauntlet;
 import com.jnape.palatable.lambda.adt.Maybe;
 import com.jnape.palatable.lambda.functions.Fn1;
 import dev.marksman.enhancediterables.ImmutableFiniteIterable;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.With;
 
 import java.time.Duration;
 import java.util.Set;
 import java.util.concurrent.Executor;
 
-@With
-@Getter
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 final class DomainTestParameters<A> {
     private final Domain<A> domain;
     private final Quantifier quantifier;
     private final ImmutableFiniteIterable<Fn1<A, Set<String>>> classifiers;
     private final Duration timeout;
     private final Maybe<Executor> executorOverride;
+
+    private DomainTestParameters(Domain<A> domain, Quantifier quantifier, ImmutableFiniteIterable<Fn1<A, Set<String>>> classifiers, Duration timeout, Maybe<Executor> executorOverride) {
+        this.domain = domain;
+        this.quantifier = quantifier;
+        this.classifiers = classifiers;
+        this.timeout = timeout;
+        this.executorOverride = executorOverride;
+    }
 
     public static <A> DomainTestParameters<A> domainTestParameters(Domain<A> domain,
                                                                    Quantifier quantifier,
@@ -32,5 +33,45 @@ final class DomainTestParameters<A> {
 
     public DomainTestParameters<A> addClassifier(Fn1<A, Set<String>> classifier) {
         return new DomainTestParameters<>(domain, quantifier, classifiers.prepend(classifier), timeout, executorOverride);
+    }
+
+    public Domain<A> getDomain() {
+        return this.domain;
+    }
+
+    public Quantifier getQuantifier() {
+        return this.quantifier;
+    }
+
+    public ImmutableFiniteIterable<Fn1<A, Set<String>>> getClassifiers() {
+        return this.classifiers;
+    }
+
+    public Duration getTimeout() {
+        return this.timeout;
+    }
+
+    public Maybe<Executor> getExecutorOverride() {
+        return this.executorOverride;
+    }
+
+    public DomainTestParameters<A> withDomain(Domain<A> domain) {
+        return this.domain == domain ? this : new DomainTestParameters<>(domain, this.quantifier, this.classifiers, this.timeout, this.executorOverride);
+    }
+
+    public DomainTestParameters<A> withQuantifier(Quantifier quantifier) {
+        return this.quantifier == quantifier ? this : new DomainTestParameters<>(this.domain, quantifier, this.classifiers, this.timeout, this.executorOverride);
+    }
+
+    public DomainTestParameters<A> withClassifiers(ImmutableFiniteIterable<Fn1<A, Set<String>>> classifiers) {
+        return this.classifiers == classifiers ? this : new DomainTestParameters<>(this.domain, this.quantifier, classifiers, this.timeout, this.executorOverride);
+    }
+
+    public DomainTestParameters<A> withTimeout(Duration timeout) {
+        return this.timeout == timeout ? this : new DomainTestParameters<>(this.domain, this.quantifier, this.classifiers, timeout, this.executorOverride);
+    }
+
+    public DomainTestParameters<A> withExecutorOverride(Maybe<Executor> executorOverride) {
+        return this.executorOverride == executorOverride ? this : new DomainTestParameters<>(this.domain, this.quantifier, this.classifiers, this.timeout, executorOverride);
     }
 }
