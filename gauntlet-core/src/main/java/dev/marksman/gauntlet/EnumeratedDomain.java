ac34394a27dc35b2eae26810662de7bb75896c1f
Kevin Schuetz <schuetzk@gmail.com>
2020-05-06 22:12:45 -0500

    Delombok EnumeratedDomain

--- gauntlet-core/src/main/java/dev/marksman/gauntlet/EnumeratedDomain.java
+++ gauntlet-core/src/main/java/dev/marksman/gauntlet/EnumeratedDomain.java
@@ -3,20 +3,19 @@ package dev.marksman.gauntlet;
 import com.jnape.palatable.lambda.functions.Fn1;
 import dev.marksman.collectionviews.ImmutableVector;
 import dev.marksman.collectionviews.Vector;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
 import java.util.Objects;
 
 import static dev.marksman.gauntlet.filter.Filter.filter;
 
-@AllArgsConstructor(access = AccessLevel.PACKAGE)
 final class EnumeratedDomain<A> implements Domain<A> {
-    @Getter
-    ImmutableVector<A> elements;
-    @Getter
-    Fn1<? super A, String> prettyPrinter;
+    private final ImmutableVector<A> elements;
+    private final Fn1<? super A, String> prettyPrinter;
+
+    EnumeratedDomain(ImmutableVector<A> elements, Fn1<? super A, String> prettyPrinter) {
+        this.elements = elements;
+        this.prettyPrinter = prettyPrinter;
+    }
 
     static <A> EnumeratedDomain<A> enumeratedDomain(Iterable<A> elements) {
         return new EnumeratedDomain<>(Vector.copyFrom(elements), Objects::toString);
@@ -30,5 +29,13 @@ final class EnumeratedDomain<A> implements Domain<A> {
     @Override
     public Domain<A> suchThat(Fn1<? super A, Boolean> predicate) {
         return new FilteredDomain<>(elements, filter(predicate), prettyPrinter);
+    }
+
+    public ImmutableVector<A> getElements() {
+        return this.elements;
+    }
+
+    public Fn1<? super A, String> getPrettyPrinter() {
+        return this.prettyPrinter;
     }
 }
