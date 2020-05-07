b75715fbe6bfef00790a934000475a028f62786f
Kevin Schuetz <schuetzk@gmail.com>
2020-05-06 21:34:30 -0500

    Delombok Reasons

--- gauntlet-core/src/main/java/dev/marksman/gauntlet/Reasons.java
+++ gauntlet-core/src/main/java/dev/marksman/gauntlet/Reasons.java
@@ -5,15 +5,13 @@ import dev.marksman.collectionviews.NonEmptyVector;
 import dev.marksman.collectionviews.NonEmptyVectorBuilder;
 import dev.marksman.collectionviews.Vector;
 import dev.marksman.enhancediterables.NonEmptyIterable;
-import lombok.AllArgsConstructor;
-import lombok.Value;
 
-import static lombok.AccessLevel.PRIVATE;
+public final class Reasons {
+    private final ImmutableNonEmptyVector<String> items;
 
-@Value
-@AllArgsConstructor(access = PRIVATE)
-public class Reasons {
-    ImmutableNonEmptyVector<String> items;
+    private Reasons(ImmutableNonEmptyVector<String> items) {
+        this.items = items;
+    }
 
     public static Reasons reasons(String primaryReason, String... additional) {
         if (additional.length == 0) {
@@ -35,4 +33,28 @@ public class Reasons {
         return items.head();
     }
 
+    public ImmutableNonEmptyVector<String> getItems() {
+        return this.items;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Reasons)) return false;
+        final Reasons other = (Reasons) o;
+        final Object this$items = this.getItems();
+        final Object other$items = other.getItems();
+        return this$items == null ? other$items == null : this$items.equals(other$items);
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $items = this.getItems();
+        result = result * PRIME + ($items == null ? 43 : $items.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "Reasons(items=" + this.getItems() + ")";
+    }
 }
