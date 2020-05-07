0dd798760cc1a0ea392f99f454995ef2593d9fd2
Kevin Schuetz <schuetzk@gmail.com>
2020-05-06 21:33:46 -0500

    Delombok GeneratorOutput

--- gauntlet-core/src/main/java/dev/marksman/gauntlet/GeneratorOutput.java
+++ gauntlet-core/src/main/java/dev/marksman/gauntlet/GeneratorOutput.java
@@ -5,18 +5,18 @@ import com.jnape.palatable.lambda.functions.Fn1;
 import com.jnape.palatable.lambda.functor.Functor;
 import dev.marksman.kraftwerk.Result;
 import dev.marksman.kraftwerk.Seed;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Value;
 
 import static com.jnape.palatable.lambda.adt.Either.left;
 import static com.jnape.palatable.lambda.adt.Either.right;
 
-@Value
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-public class GeneratorOutput<A> implements Functor<A, GeneratorOutput<?>> {
-    Seed nextState;
-    Either<SupplyFailure, A> value;
+public final class GeneratorOutput<A> implements Functor<A, GeneratorOutput<?>> {
+    private final Seed nextState;
+    private final Either<SupplyFailure, A> value;
+
+    private GeneratorOutput(Seed nextState, Either<SupplyFailure, A> value) {
+        this.nextState = nextState;
+        this.value = value;
+    }
 
     static <A> GeneratorOutput<A> generatorOutput(Seed nextState, Either<SupplyFailure, A> value) {
         return new GeneratorOutput<>(nextState, value);
@@ -41,5 +41,39 @@ public class GeneratorOutput<A> implements Functor<A, GeneratorOutput<?>> {
     @Override
     public <B> GeneratorOutput<B> fmap(Fn1<? super A, ? extends B> fn) {
         return generatorOutput(nextState, value.fmap(fn));
+    }
+
+    public Seed getNextState() {
+        return this.nextState;
+    }
+
+    public Either<SupplyFailure, A> getValue() {
+        return this.value;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof GeneratorOutput)) return false;
+        final GeneratorOutput<?> other = (GeneratorOutput<?>) o;
+        final Object this$nextState = this.getNextState();
+        final Object other$nextState = other.getNextState();
+        if (this$nextState == null ? other$nextState != null : !this$nextState.equals(other$nextState)) return false;
+        final Object this$value = this.getValue();
+        final Object other$value = other.getValue();
+        return this$value == null ? other$value == null : this$value.equals(other$value);
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $nextState = this.getNextState();
+        result = result * PRIME + ($nextState == null ? 43 : $nextState.hashCode());
+        final Object $value = this.getValue();
+        result = result * PRIME + ($value == null ? 43 : $value.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "GeneratorOutput(nextState=" + this.getNextState() + ", value=" + this.getValue() + ")";
     }
 }
