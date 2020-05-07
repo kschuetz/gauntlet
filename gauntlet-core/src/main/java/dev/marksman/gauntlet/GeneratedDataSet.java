80654d77f070a667ce95f81e2d89f8258c6210fe
Kevin Schuetz <schuetzk@gmail.com>
2020-05-06 22:18:46 -0500

    Delombok GeneratedDataSet

--- gauntlet-core/src/main/java/dev/marksman/gauntlet/GeneratedDataSet.java
+++ gauntlet-core/src/main/java/dev/marksman/gauntlet/GeneratedDataSet.java
@@ -4,22 +4,40 @@ import com.jnape.palatable.lambda.adt.Maybe;
 import dev.marksman.collectionviews.ImmutableVector;
 import dev.marksman.collectionviews.Vector;
 import dev.marksman.kraftwerk.Seed;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Value;
 
-@Value
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-class GeneratedDataSet<A> {
-    long initialSeedValue;
-    ImmutableVector<A> samples;
-    Maybe<SupplyFailure> supplyFailure;
-    Seed outputSeed;
+final class GeneratedDataSet<A> {
+    private final long initialSeedValue;
+    private final ImmutableVector<A> samples;
+    private final Maybe<SupplyFailure> supplyFailure;
+    private final Seed outputSeed;
+
+    private GeneratedDataSet(long initialSeedValue, ImmutableVector<A> samples, Maybe<SupplyFailure> supplyFailure, Seed outputSeed) {
+        this.initialSeedValue = initialSeedValue;
+        this.samples = samples;
+        this.supplyFailure = supplyFailure;
+        this.outputSeed = outputSeed;
+    }
 
     public static <A> GeneratedDataSet<A> generatedDataSet(long initialSeedValue,
                                                            Iterable<A> values,
                                                            Maybe<SupplyFailure> supplyFailure,
                                                            Seed outputSeed) {
         return new GeneratedDataSet<>(initialSeedValue, Vector.copyFrom(values), supplyFailure, outputSeed);
+    }
+
+    public long getInitialSeedValue() {
+        return this.initialSeedValue;
+    }
+
+    public ImmutableVector<A> getSamples() {
+        return this.samples;
+    }
+
+    public Maybe<SupplyFailure> getSupplyFailure() {
+        return this.supplyFailure;
+    }
+
+    public Seed getOutputSeed() {
+        return this.outputSeed;
     }
 }
