7c445859cff3cf5f9c3eeffc5e2e26fcde102254
Kevin Schuetz <schuetzk@gmail.com>
2020-05-06 21:39:02 -0500

    Delombok Counterexample

--- gauntlet-core/src/main/java/dev/marksman/gauntlet/Counterexample.java
+++ gauntlet-core/src/main/java/dev/marksman/gauntlet/Counterexample.java
@@ -1,18 +1,49 @@
 package dev.marksman.gauntlet;
 
-import lombok.AllArgsConstructor;
-import lombok.Value;
+public final class Counterexample<A> {
+    private final EvalFailure failure;
+    private final A sample;
 
-import static lombok.AccessLevel.PRIVATE;
-
-
-@Value
-@AllArgsConstructor(access = PRIVATE)
-public class Counterexample<A> {
-    EvalFailure failure;
-    A sample;
+    private Counterexample(EvalFailure failure, A sample) {
+        this.failure = failure;
+        this.sample = sample;
+    }
 
     public static <A> Counterexample<A> counterexample(EvalFailure failure, A sample) {
         return new Counterexample<>(failure, sample);
+    }
+
+    public EvalFailure getFailure() {
+        return this.failure;
+    }
+
+    public A getSample() {
+        return this.sample;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Counterexample)) return false;
+        final Counterexample<?> other = (Counterexample<?>) o;
+        final Object this$failure = this.getFailure();
+        final Object other$failure = other.getFailure();
+        if (this$failure == null ? other$failure != null : !this$failure.equals(other$failure)) return false;
+        final Object this$sample = this.getSample();
+        final Object other$sample = other.getSample();
+        return this$sample == null ? other$sample == null : this$sample.equals(other$sample);
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $failure = this.getFailure();
+        result = result * PRIME + ($failure == null ? 43 : $failure.hashCode());
+        final Object $sample = this.getSample();
+        result = result * PRIME + ($sample == null ? 43 : $sample.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "Counterexample(failure=" + this.getFailure() + ", sample=" + this.getSample() + ")";
     }
 }
