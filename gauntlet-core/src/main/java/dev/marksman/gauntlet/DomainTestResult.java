c57055a66b3808d85180621088b3fa1d95326496
Kevin Schuetz <schuetzk@gmail.com>
2020-05-06 22:15:18 -0500

    Delombok DomainTestResult

--- gauntlet-core/src/main/java/dev/marksman/gauntlet/DomainTestResult.java
+++ gauntlet-core/src/main/java/dev/marksman/gauntlet/DomainTestResult.java
@@ -1,15 +1,38 @@
 package dev.marksman.gauntlet;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Value;
+final class DomainTestResult<A> {
+    private final TestResult<A> result;
 
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Value
-public class DomainTestResult<A> {
-    TestResult<A> result;
+    private DomainTestResult(TestResult<A> result) {
+        this.result = result;
+    }
 
-    public static <A> DomainTestResult<A> domainTestResult(TestResult<A> testResult) {
+    static <A> DomainTestResult<A> domainTestResult(TestResult<A> testResult) {
         return new DomainTestResult<>(testResult);
+    }
+
+    public TestResult<A> getResult() {
+        return this.result;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof DomainTestResult)) return false;
+        final DomainTestResult<?> other = (DomainTestResult<?>) o;
+        final Object this$result = this.getResult();
+        final Object other$result = other.getResult();
+        return this$result == null ? other$result == null : this$result.equals(other$result);
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $result = this.getResult();
+        result = result * PRIME + ($result == null ? 43 : $result.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "DomainTestResult(result=" + this.getResult() + ")";
     }
 }
