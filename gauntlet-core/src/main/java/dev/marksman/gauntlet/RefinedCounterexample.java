dea21e774185fec49421f60549266e0df63576f4
Kevin Schuetz <schuetzk@gmail.com>
2020-05-06 22:11:01 -0500

    Delombok RefinedCounterexample

--- gauntlet-core/src/main/java/dev/marksman/gauntlet/RefinedCounterexample.java
+++ gauntlet-core/src/main/java/dev/marksman/gauntlet/RefinedCounterexample.java
@@ -1,18 +1,48 @@
 package dev.marksman.gauntlet;
 
-import lombok.AllArgsConstructor;
-import lombok.Value;
+public final class RefinedCounterexample<A> {
+    private final Counterexample<A> counterexample;
+    private final int shrinkCount;
 
-import static lombok.AccessLevel.PRIVATE;
-
-@Value
-@AllArgsConstructor(access = PRIVATE)
-public class RefinedCounterexample<A> {
-    Counterexample<A> counterexample;
-    int shrinkCount;
+    private RefinedCounterexample(Counterexample<A> counterexample, int shrinkCount) {
+        this.counterexample = counterexample;
+        this.shrinkCount = shrinkCount;
+    }
 
     public static <A> RefinedCounterexample<A> refinedCounterexample(Counterexample<A> counterexample,
                                                                      int shrinkCount) {
         return new RefinedCounterexample<>(counterexample, shrinkCount);
+    }
+
+    public Counterexample<A> getCounterexample() {
+        return this.counterexample;
+    }
+
+    public int getShrinkCount() {
+        return this.shrinkCount;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof RefinedCounterexample)) return false;
+        final RefinedCounterexample<?> other = (RefinedCounterexample<?>) o;
+        final Object this$counterexample = this.getCounterexample();
+        final Object other$counterexample = other.getCounterexample();
+        if (this$counterexample == null ? other$counterexample != null : !this$counterexample.equals(other$counterexample))
+            return false;
+        return this.getShrinkCount() == other.getShrinkCount();
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $counterexample = this.getCounterexample();
+        result = result * PRIME + ($counterexample == null ? 43 : $counterexample.hashCode());
+        result = result * PRIME + this.getShrinkCount();
+        return result;
+    }
+
+    public String toString() {
+        return "RefinedCounterexample(counterexample=" + this.getCounterexample() + ", shrinkCount=" + this.getShrinkCount() + ")";
     }
 }
